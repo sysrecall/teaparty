4b6d81ebd5c02fcc5773ad4dfa71e7eb0cce7b1d
sysrecall <uprojectemail@protonmail.com>
2026-05-23 01:37:25 +0600

    refactor(server): move turn server fetching logic to app, only one set of servers are created for a pair

--- server/internal/api/websocket_handler.go
+++ server/internal/api/websocket_handler.go
@@ -1,14 +1,12 @@
 package api
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/coder/websocket"
@@ -37,15 +35,17 @@ const (
 )
 
 type Room struct {
-	Id      string
-	Client1 *Client
-	Client2 *Client
+	Id          string
+	Client1     *Client
+	Client2     *Client
+	TurnServers string
 }
 
-func NewRoom() *Room {
+func NewRoom(turnServers string) *Room {
 	id := uuid.NewString()
 	return &Room{
-		Id: id,
+		Id:          id,
+		TurnServers: turnServers,
 	}
 }
 
@@ -76,104 +76,6 @@ func NewWebsocketHandler(logger *log.Logger, queue Queue) *WebsocketHandler {
 }
 
 const READ_LIMIT_BYTES = 1024 * 10
-
-type turnCredentials struct {
-	Username        string `json:"username"`
-	Password        string `json:"password"`
-	ExpiryInSeconds int    `json:"expiryInSeconds"`
-	Label           string `json:"label,omitempty"`
-	ApiKey          string `json:"apiKey"`
-}
-
-type turnServer struct {
-	Urls        string `json:"urls"`
-	Username    string `json:"username"`
-	Credentials string `json:"credentials"`
-}
-
-func (wh *WebsocketHandler) getTurnCredentials() (turnCredentials, error) {
-	// load env variables
-	domain, foundDomain := os.LookupEnv("METERED_DOMAIN")
-	if !foundDomain {
-		wh.logger.Fatal("TURN server domain does not exist in env")
-	}
-
-	turnSecret, foundTurnSecret := os.LookupEnv("METERED_SECRET_KEY")
-	if !foundTurnSecret {
-		wh.logger.Fatal("TURN server secret does not exist in env")
-
-	}
-
-	turnExipry, foundTurnExpiry := os.LookupEnv("TURN_CREDENTIALS_EXPIRY_SECONDS")
-	if !foundTurnExpiry {
-		wh.logger.Fatal("TURN server expiry does not exist in env")
-	}
-
-	// request credentials
-	url := fmt.Sprintf("https://%v/api/v1/turn/credential", domain)
-	requestUrl := fmt.Sprintf("%v?secretKey=%v", url, turnSecret)
-
-	data := map[string]string{"expiryInSeconds": turnExipry}
-	jsonData, _ := json.Marshal(data)
-
-	res, err := http.Post(requestUrl, "application/json", bytes.NewBuffer(jsonData))
-
-	if err != nil {
-		wh.logger.Printf("Error requesting turn credentials: %v", err)
-	}
-
-	defer res.Body.Close()
-
-	var turnCredentials turnCredentials
-
-	err = json.NewDecoder(res.Body).Decode(&turnCredentials)
-
-	if err != nil {
-		wh.logger.Printf("Unable to parse turn credetial response: %v", err)
-		return turnCredentials, err
-	}
-
-	if res.StatusCode != http.StatusOK {
-		return turnCredentials, fmt.Errorf("unexpected status %d", res.StatusCode)
-	}
-
-	return turnCredentials, nil
-}
-
-func (wh *WebsocketHandler) getTurnServers(apiKey string) (string, error) {
-	// load env variables
-	domain, foundDomain := os.LookupEnv("METERED_DOMAIN")
-	if !foundDomain {
-		wh.logger.Fatal("TURN server domain does not exist in env")
-	}
-
-	// request ice servers
-	url := fmt.Sprintf("https://%v/api/v1/turn/credentials", domain)
-	requestUrl := fmt.Sprintf("%v?apiKey=%v", url, apiKey)
-
-	res, err := http.Get(requestUrl)
-
-	if err != nil {
-		wh.logger.Printf("Error requesting turn servers: %v", err)
-		return "", err
-	}
-
-	defer res.Body.Close()
-
-	// map servers
-	turnServers, err := io.ReadAll(res.Body)
-	if err != nil {
-		wh.logger.Printf("Error parsing body: %v", err)
-		return "", err
-	}
-
-	if res.StatusCode != 200 {
-		wh.logger.Printf("%s", string(turnServers))
-		return "", fmt.Errorf("Invalid Request")
-	}
-
-	return string(turnServers), nil
-}
 
 func (wh *WebsocketHandler) HandleWebsocket(w http.ResponseWriter, r *http.Request) {
 	connection, err := websocket.Accept(w, r, &websocket.AcceptOptions{
@@ -227,23 +129,15 @@ func (wh *WebsocketHandler) HandleWebsocket(w http.ResponseWriter, r *http.Reque
 	relayContext, cancelRelay := context.WithCancel(context.Background())
 	defer cancelRelay()
 
-	// get turn credentials
-	turnCredentials, turnCredentialsError := wh.getTurnCredentials()
+	// send turn servers
+	if client.Room.TurnServers != "" {
+		msg, err := json.Marshal(Message{
+			MessageType:    "servers",
+			MessageContent: client.Room.TurnServers,
+		})
 
-	if turnCredentialsError == nil {
-		// get turn servers
-		turnServers, turnServersError := wh.getTurnServers(turnCredentials.ApiKey)
-
-		// send turn server list
-		if turnServersError == nil {
-			msg, err := json.Marshal(Message{
-				MessageType:    "servers",
-				MessageContent: turnServers,
-			})
-
-			if err == nil {
-				client.Conn.Write(relayContext, websocket.MessageText, msg)
-			}
+		if err == nil {
+			client.Conn.Write(relayContext, websocket.MessageText, msg)
 		}
 	}
 
--- server/internal/app/app.go
+++ server/internal/app/app.go
@@ -1,7 +1,10 @@
 package app
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -54,14 +57,15 @@ func (app *Application) MakePairs() {
 		client1 := <-app.WaitingQueue
 		client2 := <-app.WaitingQueue
 
-		// limiter := rate.NewLimiter(rate.Every(time.Second * 2), 3)
+		// assign turn servers on pair
+		turnServers := app.fetchTurnServers()
 
 		// create a room and populate the room
-
 		room := &api.Room{
-			Id:      uuid.NewString(),
-			Client1: client1,
-			Client2: client2,
+			Id:          uuid.NewString(),
+			Client1:     client1,
+			Client2:     client2,
+			TurnServers: turnServers,
 		}
 
 		client1.Room = room
@@ -77,4 +81,118 @@ func (app *Application) MakePairs() {
 		close(client2.Matched)
 
 	}
+}
+
+type turnCredentials struct {
+	Username        string `json:"username"`
+	Password        string `json:"password"`
+	ExpiryInSeconds int    `json:"expiryInSeconds"`
+	Label           string `json:"label,omitempty"`
+	ApiKey          string `json:"apiKey"`
+}
+
+type turnServer struct {
+	Urls        string `json:"urls"`
+	Username    string `json:"username"`
+	Credentials string `json:"credentials"`
+}
+
+func (app *Application) fetchTurnServers() string {
+	creds, err := app.getTurnCredentials()
+	if err != nil {
+		app.Logger.Printf("failed to get turn credentials: %v", err)
+		return ""
+	}
+
+	servers, err := app.getTurnServers(creds.ApiKey)
+	if err != nil {
+		app.Logger.Printf("failed to get turn servers: %v", err)
+		return ""
+	}
+
+	return servers
+}
+
+func (app *Application) getTurnCredentials() (turnCredentials, error) {
+	// load env variables
+	domain, foundDomain := os.LookupEnv("METERED_DOMAIN")
+	if !foundDomain {
+		app.Logger.Fatal("TURN server domain does not exist in env")
+	}
+
+	turnSecret, foundTurnSecret := os.LookupEnv("METERED_SECRET_KEY")
+	if !foundTurnSecret {
+		app.Logger.Fatal("TURN server secret does not exist in env")
+
+	}
+
+	turnExipry, foundTurnExpiry := os.LookupEnv("TURN_CREDENTIALS_EXPIRY_SECONDS")
+	if !foundTurnExpiry {
+		app.Logger.Fatal("TURN server expiry does not exist in env")
+	}
+
+	// request credentials
+	url := fmt.Sprintf("https://%v/api/v1/turn/credential", domain)
+	requestUrl := fmt.Sprintf("%v?secretKey=%v", url, turnSecret)
+
+	data := map[string]string{"expiryInSeconds": turnExipry}
+	jsonData, _ := json.Marshal(data)
+
+	res, err := http.Post(requestUrl, "application/json", bytes.NewBuffer(jsonData))
+
+	if err != nil {
+		app.Logger.Printf("Error requesting turn credentials: %v", err)
+	}
+
+	defer res.Body.Close()
+
+	var turnCredentials turnCredentials
+
+	err = json.NewDecoder(res.Body).Decode(&turnCredentials)
+
+	if err != nil {
+		app.Logger.Printf("Unable to parse turn credetial response: %v", err)
+		return turnCredentials, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return turnCredentials, fmt.Errorf("unexpected status %d", res.StatusCode)
+	}
+
+	return turnCredentials, nil
+}
+
+func (app *Application) getTurnServers(apiKey string) (string, error) {
+	// load env variables
+	domain, foundDomain := os.LookupEnv("METERED_DOMAIN")
+	if !foundDomain {
+		app.Logger.Fatal("TURN server domain does not exist in env")
+	}
+
+	// request ice servers
+	url := fmt.Sprintf("https://%v/api/v1/turn/credentials", domain)
+	requestUrl := fmt.Sprintf("%v?apiKey=%v", url, apiKey)
+
+	res, err := http.Get(requestUrl)
+
+	if err != nil {
+		app.Logger.Printf("Error requesting turn servers: %v", err)
+		return "", err
+	}
+
+	defer res.Body.Close()
+
+	// map servers
+	turnServers, err := io.ReadAll(res.Body)
+	if err != nil {
+		app.Logger.Printf("Error parsing body: %v", err)
+		return "", err
+	}
+
+	if res.StatusCode != 200 {
+		app.Logger.Printf("%s", string(turnServers))
+		return "", fmt.Errorf("Invalid Request")
+	}
+
+	return string(turnServers), nil
 }
